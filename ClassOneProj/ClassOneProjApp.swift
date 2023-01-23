5f037a93171ef0b39cb507f3f9153ff8967d194a
Christopher Kennedy <kennednu@bc.edu>
2023-01-23 17:46:24 -0500

    Initial Commit

---
+++ ClassOneProj/ClassOneProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassOneProjApp.swift
+//  ClassOneProj
+//
+//  Created by Christopher Kennedy on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassOneProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
